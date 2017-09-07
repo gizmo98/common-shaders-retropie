58aaed7d6e2f358e3fff326fdc7259af07e3d8ea
gizmo98 <atz.SE@gmx.de>
2017-09-07 15:04:28 +0200

    fix bsnes_gamma_ramp.glsl

--- shaders/bsnes_gamma_ramp.glsl
+++ shaders/bsnes_gamma_ramp.glsl
@@ -58,16 +58,21 @@ void main()
 #elif defined(FRAGMENT)
 
 #if __VERSION__ >= 130
-#define COMPAT_VARYING out
-#define COMPAT_ATTRIBUTE in
+#define COMPAT_VARYING in
 #define COMPAT_TEXTURE texture
+out vec4 FragColor;
 #else
-#define COMPAT_VARYING varying 
-#define COMPAT_ATTRIBUTE attribute 
+#define COMPAT_VARYING varying
+#define FragColor gl_FragColor
 #define COMPAT_TEXTURE texture2D
 #endif
 
 #ifdef GL_ES
+#ifdef GL_FRAGMENT_PRECISION_HIGH
+precision highp float;
+#else
+precision mediump float;
+#endif
 #define COMPAT_PRECISION mediump
 #else
 #define COMPAT_PRECISION
